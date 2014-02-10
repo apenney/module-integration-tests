fbd5c6d734e2a5dec0013de3898552446fd88260
Ashley Penney <ashley.penney@puppetlabs.com>
2014-02-10 17:25:10 -0500

    Bunch more tweaks.

--- spec/acceptance/test_all_modules_spec.rb
+++ spec/acceptance/test_all_modules_spec.rb
@@ -1,32 +1,31 @@
 require 'spec_helper_acceptance'
 
-def backup_site
-  on master, shell("cp #{master['puppetpath']}/manifests/site.pp /tmp/site.pp")
-end
-
-def create_site(pp)
-  test = master['puppetpath']
-  on master, create_remote_file(master, File.join(test, "manifests", "site.pp"), pp)
-end
-
-def restore_site
-  on master, shell("cp #{master['puppetpath']}/manifests/site.pp /tmp/site.pp")
-end
-
-def run_agent
-  puppet('agent')
-end
-
 describe 'including all modules' do
+  case fact('osfamily')
+  when 'RedHat'
+    mysql_service_name     = 'mysqld'
+    apache_service_name    = 'httpd'
+  else
+    mysql_service_name     = 'mysql'
+    apache_service_name    = 'apache2'
+  end
+
   it 'includes all the modules' do
     pp = <<-EOS
       include '::apache'
       include '::ntp'
       include '::apache'
-      include '::mysql'
-      include '::postgresql'
-      include '::registry'
-      include '::concat'
+      include '::mysql::server'
+      include '::postgresql::server'
+
+      concat { '/tmp/file':
+        ensure => present,
+      }
+
+      concat::fragment { 'tmpfile':
+        target  => '/tmp/file',
+        content => 'test',
+      }
     EOS
 
     if fact('osfamily') == 'Debian'
@@ -39,7 +38,19 @@ describe 'including all modules' do
     restore_site
   end
 
-  describe package('httpd'), :node => 'agent' do
-    it { should be_installed }
+  describe service('ntp'), :node => 'agent' do
+    it { should be_running }
+  end
+
+  describe service(apache_service_name), :node => 'agent' do
+    it { should be_running }
+  end
+
+  describe service(mysql_service_name), :node => 'agent' do
+    it { should be_running }
+  end
+
+  describe file('/tmp/file'), :node => 'agent' do
+    it { should be_file }
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,17 +1,37 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 
-hosts.each do |host|
-  if host['platform'] =~ /debian/
-    on host, 'echo \'export PATH=/var/lib/gems/1.8/bin/:${PATH}\' >> ~/.bashrc'
-  end
-  if host.is_pe?
-    if host['platform'] =~ /el/
-      on master, shell('iptables -I INPUT 4 -p tcp -m state --state NEW -m tcp --dport 8140 -j ACCEPT'), { :acceptable_exit_codes => [0,1] }
+def backup_site
+  on master, shell("cp #{master['puppetpath']}/manifests/site.pp /tmp/site.pp")
+end
+
+def create_site(pp)
+  test = master['puppetpath']
+  on master, create_remote_file(master, File.join(test, "manifests", "site.pp"), pp)
+end
+
+def restore_site
+  on master, shell("cp #{master['puppetpath']}/manifests/site.pp /tmp/SITE")
+  on master, shell("cp /tmp/site.pp #{master['puppetpath']}/manifests/site.pp")
+end
+
+def run_agent
+  on agents, puppet('agent -tv')
+end
+
+unless ENV['RS_PROVISION'] == 'no'
+  hosts.each do |host|
+    if host['platform'] =~ /debian/
+      on host, 'echo \'export PATH=/var/lib/gems/1.8/bin/:${PATH}\' >> ~/.bashrc'
     end
-    install_pe
-  else
-    install_puppet
+    if host.is_pe?
+      if host['platform'] =~ /el/
+        on master, shell('iptables -I INPUT 4 -p tcp -m state --state NEW -m tcp --dport 8140 -j ACCEPT'), { :acceptable_exit_codes => [0,1] }
+      end
+      install_pe
+    else
+      install_puppet
+    end
   end
 end
 
@@ -22,18 +42,20 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    install_options = "--modulepath #{master['distmoduledir']} --ignore-dependencies --force"
-    on master, puppet('module','install','puppetlabs-ntp', install_options),        { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-firewall', install_options),   { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-apt', install_options),        { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-apache', install_options),     { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-mysql', install_options),      { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-postgresql', install_options), { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-registry', install_options),   { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-reboot', install_options),     { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-inifile', install_options),    { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-java_ks', install_options),    { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-concat', install_options),     { :acceptable_exit_codes => [0,1] }
-    on master, puppet('module','install','puppetlabs-stdlib', install_options),     { :acceptable_exit_codes => [0,1] }
+    unless ENV['RS_PROVISION'] == 'no'
+      install_options = "--modulepath #{master['distmoduledir']} --ignore-dependencies --force"
+      on master, puppet('module','install','puppetlabs-ntp', install_options),        { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-firewall', install_options),   { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-apt', install_options),        { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-apache', install_options),     { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-mysql', install_options),      { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-postgresql', install_options), { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-registry', install_options),   { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-reboot', install_options),     { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-inifile', install_options),    { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-java_ks', install_options),    { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-concat', '--version 1.1.0-rc1', install_options), { :acceptable_exit_codes => [0,1] }
+      on master, puppet('module','install','puppetlabs-stdlib', install_options),     { :acceptable_exit_codes => [0,1] }
+    end
   end
 end
